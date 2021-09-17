3f74371f5b4ac8ca732bd60e8ac6146d78594b7b
koebe1 <koe.be.pay@web.de>
2021-09-17 12:02:54 +0200

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+/* eslint-env node */
+
+import DatabaseImporter from "./lib/DatabaseImporter.js";
+import MovieParser from "./lib/MovieParser.js";
+
+let dbFile = process.argv[2],
+    dataPath = process.argv[3];
+
+
+    // callback functin that gets invoked for every movie 
+function onMovieParsed(movie) {
+    DatabaseImporter.importMovie(movie);
+}
+
+function onDatabaseReady() {
+    MovieParser.setMovieParserListener(onMovieParsed);
+    MovieParser.parseMoviesFrom(dataPath);
+}
+
+DatabaseImporter.prepare(dbFile, onDatabaseReady);
